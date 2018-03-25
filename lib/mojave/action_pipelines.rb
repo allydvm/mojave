bf4fc11597aac9c33232734a890a828a3383c5e3
Tad Thorley <phaedryx@gmail.com>
2018-03-25 16:33:55 -0600

    more action pipeline stubbing

--- lib/mojave/action_pipelines.rb
+++ lib/mojave/action_pipelines.rb
@@ -1,24 +1,65 @@
 module Mojave
   module ActionPipelines
+    include Mojava::PipelineMethods
+
+    protected
+
+    def index_pipeline
+      []
+    end
+
+    def show_pipeline
+      []
+    end
+
+    def new_pipeline
+      []
+    end
+
+    def edit_pipeline
+      []
+    end
+
+    def create_pipeline
+      []
+    end
+
+    def update_pipeline
+      []
+    end
+
+    def destroy_pipeline
+      []
+    end
+
+    public
+
     def index
+      index_pipeline.each { |method| self.send method }
     end
 
     def show
+      show_pipeline.each { |method| self.send method }
     end
 
     def new
+      new_pipeline.each { |method| self.send method }
     end
 
     def edit
+      edit_pipeline.each { |method| self.send method }
     end
 
     def create
+      create_pipeline.each { |method| self.send method }
     end
 
     def update
+      update_pipeline.each { |method| self.send method }
     end
 
     def destroy
+      destroy_pipeline.each { |method| self.send method }
     end
   end
 end
